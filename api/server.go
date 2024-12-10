a3506b6f3d9a23767a3b8a897f98b04c4e02f52f
Alper <demir.alper@outlook.com.tr>
2024-12-10 22:11:09 +0300

    mock db

--- api/server.go
+++ api/server.go
@@ -5,13 +5,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+
 type Server struct {
-	store *db.Store
+	store db.Store
 	router *gin.Engine
 }
 
 
-func NewServer(store *db.Store) *Server {
+func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
