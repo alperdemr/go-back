665995d41e7331c0954e3d038d22698e449c8b74
Alper <demir.alper@outlook.com.tr>
2024-12-11 09:50:16 +0300

    users api add

--- api/account.go
+++ api/account.go
@@ -6,6 +6,7 @@ import (
 
 	db "github.com/alperdemr/go-back/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 )
 
 type CreateAccountRequest struct {
@@ -30,8 +31,16 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	account,err := server.store.CreateAccount(ctx,arg)
 	if err != nil {
+		if pqErr,ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation","unique_violation":
+				ctx.JSON(http.StatusForbidden,errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError,errorResponse(err))
-		return
+			return
+		
 	}
 
 	ctx.JSON(http.StatusOK,account)
