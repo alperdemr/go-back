0054a352ccf51a01bd8bedce017bf7d32a2d5c75
Alper <demir.alper@outlook.com.tr>
2024-12-12 14:09:53 +0300

    ready to deploy

--- api/account.go
+++ api/account.go
@@ -2,15 +2,16 @@ package api
 
 import (
 	"database/sql"
+	"errors"
 	"net/http"
 
 	db "github.com/alperdemr/go-back/db/sqlc"
+	"github.com/alperdemr/go-back/token"
 	"github.com/gin-gonic/gin"
 	"github.com/lib/pq"
 )
 
 type CreateAccountRequest struct {
-	Owner     string    `json:"owner" binding:"required"`
 	Currency  string    `json:"currency" binding:"required,currency"`
 }
 
@@ -23,8 +24,9 @@ func (server *Server) createAccount(ctx *gin.Context) {
 		return
 	}
 
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
 	arg := db.CreateAccountParams{
-		Owner: req.Owner,
+		Owner: authPayload.Username,
 		Currency: req.Currency,
 		Balance: 0,
 	}
@@ -58,6 +60,7 @@ func(server *Server) getAccount(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest,errorResponse(err))
 		return
 	}
+
 	account,err := server.store.GetAccount(ctx,req.ID)
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -67,6 +70,14 @@ func(server *Server) getAccount(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError,errorResponse(err))
 		return
 	}
+
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+	if account.Owner != authPayload.Username {
+		err := errors.New("account doesn't belong to the authenticated user")
+		ctx.JSON(http.StatusUnauthorized,errorResponse(err))
+		return
+	}
+
 	ctx.JSON(http.StatusOK,account)
 }
 
@@ -83,7 +94,10 @@ func(server *Server) listAccount(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest,errorResponse(err))
 		return
 	}
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+
 	accounts,err := server.store.ListAccounts(ctx,db.ListAccountsParams{
+		Owner: authPayload.Username,
 		Limit: req.PageSize,
 		Offset: (req.PageID - 1) * req.PageSize,
 	})
