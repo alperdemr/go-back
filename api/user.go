d68705b2e38c921353b298dc666e7dacc4493060
Alper <demir.alper@outlook.com.tr>
2024-12-16 10:50:20 +0300

    refresh token add

--- api/user.go
+++ api/user.go
@@ -8,6 +8,7 @@ import (
 	db "github.com/alperdemr/go-back/db/sqlc"
 	"github.com/alperdemr/go-back/util"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/lib/pq"
 )
 
@@ -84,7 +85,11 @@ type loginUserRequest struct {
 }
 
 type loginUserResponse struct {
+	SessionID uuid.UUID `json:"session_id"`
 	AccessToken string `json:"access_token"`
+	AccessTokenExpiresAt time.Time `json:"access_token_expires_at"`
+	RefreshToken string `json:"refresh_token"`
+	RefreshTokenExpiresAt time.Time `json:"refresh_token_expires_at"`
 	User userResponse `json:"user"`
 }
 
@@ -112,7 +117,28 @@ func (server *Server) loginUser(ctx *gin.Context) {
 		return
 	}
 
-	accessToken,_,err := server.tokenMaker.CreateToken(user.Username,server.config.AccessTokenDuration)
+	accessToken,accessPayload,err := server.tokenMaker.CreateToken(user.Username,server.config.AccessTokenDuration)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError,errorResponse(err))
+		return
+	}
+
+	refreshToken,refreshPayload,err := server.tokenMaker.CreateToken(user.Username,server.config.RefreshTokenDuration)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError,errorResponse(err))
+		return
+	}
+
+	session,err := server.store.CreateSession(ctx,db.CreateSessionParams{
+		ID: refreshPayload.ID,
+		Username: user.Username,
+		RefreshToken: refreshToken,
+		UserAgent: ctx.Request.UserAgent(),
+		ClientIp: ctx.ClientIP(),
+		IsBlocked: false,
+		ExpiredAt: refreshPayload.ExpiredAt,
+	})
 
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError,errorResponse(err))
@@ -120,7 +146,11 @@ func (server *Server) loginUser(ctx *gin.Context) {
 	}
 
 	rsp := loginUserResponse{
+		SessionID: session.ID,
 		AccessToken:accessToken,
+		AccessTokenExpiresAt: accessPayload.ExpiredAt,
+		RefreshToken: refreshToken,
+		RefreshTokenExpiresAt: refreshPayload.ExpiredAt,
 		User:newUserResponse(user),
 	}
 
