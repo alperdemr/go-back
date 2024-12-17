39117b65243b7c6f79b048155aad02a794086424
Alper <demir.alper@outlook.com.tr>
2024-12-17 16:07:32 +0300

    build grpc update api with optional parameters

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 	"time"
 
@@ -53,4 +54,94 @@ func TestGetUser(t *testing.T) {
 	require.Equal(t,user1.Email,user2.Email)
 	require.WithinDuration(t,user1.PasswordChangedAt,user2.PasswordChangedAt,time.Second)
 	require.WithinDuration(t,user1.CreatedAt,user2.CreatedAt,time.Second)
+}
+
+func TestUpdateUserOnlyFullName(t *testing.T) {
+	user1 := createRandomUser(t)
+	newFullName := util.RandomOwner()
+
+	updatedUser,err := testQueries.UpdateUser(context.Background(),UpdateUserParams{
+		Username: user1.Username,
+		FullName: sql.NullString{
+			String: newFullName,
+			Valid: true,
+		},
+	})
+	require.NoError(t,err)
+	require.NotEqual(t,user1.FullName,updatedUser.FullName)
+	require.Equal(t,newFullName,updatedUser.FullName)
+	require.Equal(t,user1.Email,updatedUser.Email)
+	require.Equal(t,user1.HashedPassword,updatedUser.HashedPassword)
+}
+
+func TestUpdateUserOnlyEmail(t *testing.T) {
+	user1 := createRandomUser(t)
+	newEmail := util.RandomEmail()
+
+	updatedUser,err := testQueries.UpdateUser(context.Background(),UpdateUserParams{
+		Username: user1.Username,
+		Email: sql.NullString{
+			String: newEmail,
+			Valid: true,
+		},
+	})
+	require.NoError(t,err)
+	require.NotEqual(t,user1.Email,updatedUser.Email)
+	require.Equal(t,newEmail,updatedUser.Email)
+	require.Equal(t,user1.FullName,updatedUser.FullName)
+	require.Equal(t,user1.HashedPassword,updatedUser.HashedPassword)
+}
+
+func TestUpdateUserOnlyPassword(t *testing.T) {
+	user1 := createRandomUser(t)
+	newPassword := util.RandomString(6)
+	newHashedPassword,err :=  util.HashPassword(newPassword)
+	require.NoError(t,err)
+
+	updatedUser,err := testQueries.UpdateUser(context.Background(),UpdateUserParams{
+		Username: user1.Username,
+		HashedPassword: sql.NullString{
+			String: newHashedPassword,
+			Valid: true,
+		},
+	})
+	require.NoError(t,err)
+	require.NotEqual(t,user1.HashedPassword,updatedUser.HashedPassword)
+	require.Equal(t,newHashedPassword,updatedUser.HashedPassword)
+	require.Equal(t,user1.FullName,updatedUser.FullName)
+	require.Equal(t,user1.Email,updatedUser.Email)
+}
+
+func TestUpdateUserAllFields(t *testing.T) {
+	user1 := createRandomUser(t)
+	newFullName:=util.RandomOwner()
+	newEmail := util.RandomEmail()
+	newPassword := util.RandomString(6)
+	newHashedPassword,err :=  util.HashPassword(newPassword)
+	require.NoError(t,err)
+	
+
+	updatedUser,err := testQueries.UpdateUser(context.Background(),UpdateUserParams{
+		Username: user1.Username,
+		FullName: sql.NullString{
+			String: newFullName,
+			Valid: true,
+		},
+		Email: sql.NullString{
+			String: newEmail,
+			Valid: true,
+		},
+		HashedPassword: sql.NullString{
+			String: newHashedPassword,
+			Valid: true,
+		},
+	})
+	require.NoError(t,err)
+	require.NotEqual(t,user1.HashedPassword,updatedUser.HashedPassword)
+	require.Equal(t,newHashedPassword,updatedUser.HashedPassword)
+	require.NotEqual(t,user1.Email,updatedUser.Email)
+	require.Equal(t,newEmail,updatedUser.Email)
+	require.NotEqual(t,user1.FullName,updatedUser.FullName)
+	require.Equal(t,newFullName,updatedUser.FullName)
+	
 }
--- pb/service_go_back_grpc.pb.go
+++ pb/service_go_back_grpc.pb.go
@@ -21,6 +21,7 @@ const _ = grpc.SupportPackageIsVersion9
 const (
 	GoBack_CreateUser_FullMethodName = "/pb.GoBack/CreateUser"
 	GoBack_LoginUser_FullMethodName  = "/pb.GoBack/LoginUser"
+	GoBack_UpdateUser_FullMethodName = "/pb.GoBack/UpdateUser"
 )
 
 // GoBackClient is the client API for GoBack service.
@@ -29,6 +30,7 @@ const (
 type GoBackClient interface {
 	CreateUser(ctx context.Context, in *CreateUserRequest, opts ...grpc.CallOption) (*CreateUserResponse, error)
 	LoginUser(ctx context.Context, in *LoginUserRequest, opts ...grpc.CallOption) (*LoginUserResponse, error)
+	UpdateUser(ctx context.Context, in *UpdateUserRequest, opts ...grpc.CallOption) (*UpdateUserResponse, error)
 }
 
 type goBackClient struct {
@@ -59,12 +61,23 @@ func (c *goBackClient) LoginUser(ctx context.Context, in *LoginUserRequest, opts
 	return out, nil
 }
 
+func (c *goBackClient) UpdateUser(ctx context.Context, in *UpdateUserRequest, opts ...grpc.CallOption) (*UpdateUserResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(UpdateUserResponse)
+	err := c.cc.Invoke(ctx, GoBack_UpdateUser_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // GoBackServer is the server API for GoBack service.
 // All implementations must embed UnimplementedGoBackServer
 // for forward compatibility.
 type GoBackServer interface {
 	CreateUser(context.Context, *CreateUserRequest) (*CreateUserResponse, error)
 	LoginUser(context.Context, *LoginUserRequest) (*LoginUserResponse, error)
+	UpdateUser(context.Context, *UpdateUserRequest) (*UpdateUserResponse, error)
 	mustEmbedUnimplementedGoBackServer()
 }
 
@@ -80,6 +93,9 @@ func (UnimplementedGoBackServer) CreateUser(context.Context, *CreateUserRequest)
 }
 func (UnimplementedGoBackServer) LoginUser(context.Context, *LoginUserRequest) (*LoginUserResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method LoginUser not implemented")
+}
+func (UnimplementedGoBackServer) UpdateUser(context.Context, *UpdateUserRequest) (*UpdateUserResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UpdateUser not implemented")
 }
 func (UnimplementedGoBackServer) mustEmbedUnimplementedGoBackServer() {}
 func (UnimplementedGoBackServer) testEmbeddedByValue()                {}
@@ -138,6 +154,24 @@ func _GoBack_LoginUser_Handler(srv interface{}, ctx context.Context, dec func(in
 	return interceptor(ctx, in, info, handler)
 }
 
+func _GoBack_UpdateUser_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UpdateUserRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(GoBackServer).UpdateUser(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: GoBack_UpdateUser_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(GoBackServer).UpdateUser(ctx, req.(*UpdateUserRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // GoBack_ServiceDesc is the grpc.ServiceDesc for GoBack service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -152,6 +186,10 @@ var GoBack_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "LoginUser",
 			Handler:    _GoBack_LoginUser_Handler,
+		},
+		{
+			MethodName: "UpdateUser",
+			Handler:    _GoBack_UpdateUser_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
